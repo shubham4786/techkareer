e10e88649269caeee88f3a0aded9d3eacd5e594c
Gautam Raj <gautamraj996@gmail.com>
2024-04-06 07:04:58 +0530

    modify title and add favicon and open graph image

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Tech-Kareer",
+  title: "TechKareer",
   description: "The only app you need for hiring",
 };
 
