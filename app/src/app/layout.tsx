f4256943941e9a26a6c8020c42717f6983d4ef03
Gautam Raj <gautamraj996@gmail.com>
2024-04-06 22:13:58 +0530

    correct title and change og image

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TechKareer",
+  title: "Techkareer",
   description: "The only app you need for hiring",
 };
 
