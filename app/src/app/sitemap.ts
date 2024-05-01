9a10c34d0e6ddf80777cfd3240df0b78d94edd98
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-01 14:52:50 +0530

    refactor: use constant for URL

--- app/src/app/sitemap.ts
+++ app/src/app/sitemap.ts
@@ -1,9 +1,10 @@
+import { BASE_URL } from "@/utils/constants";
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://techkareer-lemon.vercel.app",
+      url: BASE_URL,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
