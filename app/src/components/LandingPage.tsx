0c1d5fd51dd856812def1837df63e9924cfcc995
Harsh Agrawal <itsharshag@gmail.com>
2024-05-01 08:25:50 +0530

    Update testimonials

--- app/src/components/LandingPage.tsx
+++ app/src/components/LandingPage.tsx
@@ -8,23 +8,22 @@ import { Companies } from "./companies";
 import { Sponsor } from "./sponsor";
 import { Footer } from "./footer";
 
-
 const LandingPage = () => {
   return (
     <main className="min-h-screen relative scroll-smooth ">
-    <Navbar />
-    <Welcome/>
-    <InfiniteMovingCards
+      <Navbar />
+      <Welcome />
+      <InfiniteMovingCards
         items={testimonials}
         className="mt-0"
         direction="left"
         speed="slow"
       />
-      <Feature/>
-      <Oppurtunities/>
-      <Companies/>
-      <Sponsor/>
-      <Footer/>
+      <Feature />
+      <Oppurtunities />
+      <Companies />
+      <Sponsor />
+      <Footer />
     </main>
   );
 };
