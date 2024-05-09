98c6be39debc8977bb4d8af35d81913ec5f92969
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-05-09 13:15:22 +0530

    feat: added jobseeker name as navbar

--- app/src/app/(MainLayout)/(NonFunctionalRightBar)/jobseekers/[id]/page.tsx
+++ app/src/app/(MainLayout)/(NonFunctionalRightBar)/jobseekers/[id]/page.tsx
@@ -3,12 +3,15 @@ import { BottomBar, Navbar } from "@/components/components";
 import JobseekerDetails from "@/components/JobSeekerDetailNew/JobSeekerDetailNew";
 import { useFetchSingleJobseekers } from "@/hooks/useJobseekerData";
 import React from "react";
-
+import {MoveRight } from "lucide-react";
 function JobseekerDetailsPage({ params }: { params: { id: string }}) {
   const { data: jobseeker, isLoading } = useFetchSingleJobseekers(params.id);
   return (
     <>
-      <Navbar>@{params.id}</Navbar>
+    {
+      jobseeker?.id && <Navbar>{jobseeker?.name ? <div className="flex justify-start items-center gap-4"><p>{jobseeker.name} </p><MoveRight size={15}/> </div> : `Talent Id: ${jobseeker?.id}`}</Navbar>
+    }
+      
       <div className="scrollable-content-wrapper max-sm:h-[80vh] h-[90vh] w-full flex justify-center">
         {jobseeker && <JobseekerDetails jobseeker={jobseeker} isLoading={isLoading}></JobseekerDetails>}
       </div>
