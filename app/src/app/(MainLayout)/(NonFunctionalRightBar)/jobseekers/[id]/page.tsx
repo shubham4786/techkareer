d9a12c39fdccdf61804cf991d5e318577ec7f485
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-05-09 13:07:19 +0530

    feat: improved the ui of jobseeker/id route

--- app/src/app/(MainLayout)/(NonFunctionalRightBar)/jobseekers/[id]/page.tsx
+++ app/src/app/(MainLayout)/(NonFunctionalRightBar)/jobseekers/[id]/page.tsx
@@ -1,17 +1,18 @@
+'use client'
 import { BottomBar, Navbar } from "@/components/components";
 import JobseekerDetails from "@/components/JobSeekerDetailNew/JobSeekerDetailNew";
+import { useFetchSingleJobseekers } from "@/hooks/useJobseekerData";
 import React from "react";
 
 function JobseekerDetailsPage({ params }: { params: { id: string }}) {
-
+  const { data: jobseeker, isLoading } = useFetchSingleJobseekers(params.id);
   return (
     <>
       <Navbar>@{params.id}</Navbar>
       <div className="scrollable-content-wrapper max-sm:h-[80vh] h-[90vh] w-full flex justify-center">
-      <JobseekerDetails id={params.id}></JobseekerDetails>
+        {jobseeker && <JobseekerDetails jobseeker={jobseeker} isLoading={isLoading}></JobseekerDetails>}
       </div>
       <BottomBar></BottomBar>
-
     </>
   );
 }
