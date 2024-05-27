f6a7b2952b44918aee3e222efdf4e6008d93f141
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-05-27 10:32:54 +0530

    refactor: login -> LOGIN

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -87,7 +87,7 @@ export const Navbar = () => {
                 whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
                 whileTap={{ scale: 0.95 }}
             >
-                <p className="text-xs md:text-sm text-black">Login</p>
+                <p className="text-xs md:text-sm text-black">LOGIN</p>
             </motion.button>
         </Link>
                }
