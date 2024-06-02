3fe0b7152dac4fabf0c3a6656d9d7b32493990df
shubham4786 <shubhambhanarkar4786@gmail.com>
2024-06-02 13:18:50 +0530

    logo link to home page

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -13,8 +13,8 @@ import { signOut } from 'next-auth/react'
 import { useRouter } from "next/navigation"
 import { Loader } from "lucide-react"
 export const Navbar = () => {
-    const {  status } = useUser();
-    const [loggingOut , setLoggingOut] = useState(false)    
+    const { status } = useUser();
+    const [loggingOut, setLoggingOut] = useState(false)
     const router = useRouter()
     let pathname = usePathname() || "/";
     return (
@@ -24,14 +24,15 @@ export const Navbar = () => {
                 animate={{ opacity: 1 }} // Animate opacity to 1
                 transition={{ duration: 0.8 }}
                 className="flex  justify-between items-center w-[1300px] md:px-6 md:ml-8 px-4 ">
-                    <div className=" w-[160px] md:w-[220px]">
+                <div className=" w-[160px] md:w-[220px]">
                     <Image
+                        className="cursor-pointer"
                         src={logo}
                         alt="TechKareer"
-                        
-                        />
-                    </div>
-  
+                        onClick={() => router.push("/")}
+                    />
+                </div>
+
                 <div className="md:flex flex-row justify-center items-center gap-4 hidden">
                     {
                         NavLinks.map((link, index) => (
@@ -48,52 +49,53 @@ export const Navbar = () => {
                     }
                 </div>
                 <div className=" flex flex-row gap-5"
->
-                <ReactLink
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    to={"opportunities"}>
-                    <motion.button className="bg-[#15151f] px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full  font-bold tracking-wider"
-                        whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
-                        whileTap={{ scale: 0.95 }}
-                    >
-                        <p className="text-xs md:text-sm">OPPORTUNITIES</p>
-                    </motion.button>
-                </ReactLink>
-               {
-                status ? 
-                <motion.button 
-                onClick={() => {
-                    setLoggingOut(true)
-                    signOut({ redirect: false }).then(() => {
-                    router.push("/"); 
-               
-                  });}}
-                className="bg-white/90 px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full   font-bold tracking-wider "
-                    whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
-                    whileTap={{ scale: 0.95 }}
                 >
-                    <p className="text-xs md:text-sm text-black">
+                    <ReactLink
+                        spy={true}
+                        smooth={true}
+                        duration={500}
+                        to={"opportunities"}>
+                        <motion.button className="bg-[#15151f] px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full  font-bold tracking-wider"
+                            whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            <p className="text-xs md:text-sm">OPPORTUNITIES</p>
+                        </motion.button>
+                    </ReactLink>
+                    {
+                        status ?
+                            <motion.button
+                                onClick={() => {
+                                    setLoggingOut(true)
+                                    signOut({ redirect: false }).then(() => {
+                                        router.push("/");
 
-                        {
-                            loggingOut ? <Loader className="animate-spin"/> : "Logout"
-                        }
-                    </p>
-                </motion.button>
-            : 
-            <Link href={'/signin'}>
-            <motion.button className="bg-white/90 px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full   font-bold tracking-wider"
-                whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
-                whileTap={{ scale: 0.95 }}
-            >
-                <p className="text-xs md:text-sm text-black">LOGIN</p>
-            </motion.button>
-        </Link>
-               }
+                                    });
+                                }}
+                                className="bg-white/90 px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full   font-bold tracking-wider "
+                                whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
+                                whileTap={{ scale: 0.95 }}
+                            >
+                                <p className="text-xs md:text-sm text-black">
+
+                                    {
+                                        loggingOut ? <Loader className="animate-spin" /> : "Logout"
+                                    }
+                                </p>
+                            </motion.button>
+                            :
+                            <Link href={'/signin'}>
+                                <motion.button className="bg-white/90 px-4 py-2 md:px-6 md:py-3 border-[.1px] border-solid border-gray-200/10 rounded-full   font-bold tracking-wider"
+                                    whileHover={{ backgroundColor: "#F9F9F9", color: "#000" }}
+                                    whileTap={{ scale: 0.95 }}
+                                >
+                                    <p className="text-xs md:text-sm text-black">LOGIN</p>
+                                </motion.button>
+                            </Link>
+                    }
 
                 </div>
-             
+
             </motion.div>
         </nav>
     )
